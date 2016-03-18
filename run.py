18b5613befaf14d69cd1460d71a9c2e6083df455
Cookie <cookie.io@openmailbox.org>
2016-03-18 23:13:10 +0100

    Redirect uris

--- run.py
+++ run.py
@@ -13,17 +13,18 @@ req = requests.post(url='https://discordapp.com/api/auth/login', json=payload)
 if req.status_code != 200:
     print('Error :/')
     exit(1)
-
 token = req.json()['token']
 app_name = input('Application name : ')
 description = input('Application description : ')
-redirect_uri = input('Redirect URI : ')
+redirect_uri1 = input('Redirect URI 1 : ')
+redirect_uri2 = input('Redirect URI 2 : ')
+
 
 headers = {'Authorization': token}
 payload = {
     'name': app_name,
     'description': description,
-    'redirect_uris': [redirect_uri]
+    'redirect_uris': [redirect_uri1, redirect_uri2]
 }
 req = requests.post(url='https://discordapp.com/api/oauth2/applications', \
                    headers=headers,
